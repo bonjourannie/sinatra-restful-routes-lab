997b1941e733c3be063eaacaae4648e8f29c7865
Annie Truex <artruex@gmail.com>
2019-10-27 00:54:05 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-#require '.config/environment'
+require '.config/environment'
 
 class ApplicationController < Sinatra::Base
   configure do
@@ -10,12 +10,12 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  get '/recipes' do #loads index page
+  get '/recipes' do 
     @recipes = Recipe.all
     erb :index
   end
 
-  get '/recipes/:id' do  #loads show page
+  get '/recipes/:id' do  
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  patch '/recipes/:id' do  #updates a recipe
+  patch '/recipes/:id' do  
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
@@ -34,12 +34,12 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  post '/recipes' do  #creates a recipe
+  post '/recipes' do  
     @recipe = Recipe.create(params)
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  delete '/recipes/:id' do #destroy action
+  delete '/recipes/:id' do 
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
     redirect to '/recipes'
